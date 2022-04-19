4cb26dfd3affb5b8437d910ba0f956e6f56c25f3
Sy Fontenot <jdf52@students.uwf.edu>
2022-04-19 00:34:28 -0500

    Adding class

---
+++ restaurant-recommender-app/src/main/kotlin/Recommender.kt
@@ -0,0 +1,18 @@
+import tornadofx.launch
+
+class Recommender {
+
+    fun main(args: Array<String>) {
+        println(temps)
+    }
+
+    class temp(m: Int) {
+        val max = m
+        val cur = 0
+        val time = arrayOf<Double>(1.0, 1.0, 1.0)
+    }
+
+    val temps = arrayOf<temp>(temp(3), temp(4))
+
+
+}
