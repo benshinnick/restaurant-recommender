d25dc336fb2cc61cb3a7c228aef57da06b7d32c5
Angelica Jones <70334083+amjelly@users.noreply.github.com>
2022-04-20 16:36:13 -0500

    Updated reading to fix address error

--- restaurant-recommender-app/src/main/kotlin/RestaurantData.kt
+++ restaurant-recommender-app/src/main/kotlin/RestaurantData.kt
@@ -35,19 +35,33 @@ class RestaurantData {
             newRestaurant.rating = tokens[++count].toInt()
             newRestaurant.location = tokens[++count]
 
-            newRestaurant.description = tokens[++count]
 
-            count++
-            while(count < tokens.size) {
-                if (tokens[count].contains("\"")) {
-                    newRestaurant.description += tokens[count]
-                    break
-                }
-                newRestaurant.description += tokens[count]
-                count++
+            if (newRestaurant.location.contains("\"")) {
+                    count++
+                    while(count < tokens.size) {
+                        if (tokens[count].contains("\"")) {
+                            newRestaurant.description += tokens[count]
+                            break
+                        }
+                        newRestaurant.description += tokens[count]
+                        count++
+                    }
             }
 
-            restaurants.add(newRestaurant)
-        }
+                newRestaurant.description = tokens[++count]
+
+                count++
+                while(count < tokens.size) {
+                        if (tokens[count].contains("\"")) {
+                            newRestaurant.description += tokens[count]
+                            break
+                        }
+                        newRestaurant.description += tokens[count]
+                        count++
+                    }
+
+                    restaurants.add(newRestaurant)
+            }
+
     }
 }
